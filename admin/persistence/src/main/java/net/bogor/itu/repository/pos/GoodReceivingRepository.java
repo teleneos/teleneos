a2fdc5be2df17763cfb5a4a22685754c267bd003
Dias Arifin <diaxz@madcoder.org>
2013-04-25 08:05:24 +0000

    resolve JERUKKEPOK-73
    git-svn-id: http://www.madcoder.org/svn/jerukkeprok/trunk@134 0f5c9f49-3e71-46a8-8949-fce9018a0e12

--- admin/persistence/src/main/java/net/bogor/itu/repository/pos/GoodReceivingRepository.java
+++ admin/persistence/src/main/java/net/bogor/itu/repository/pos/GoodReceivingRepository.java
@@ -5,7 +5,6 @@ import net.bogor.itu.persistence.PersistenceRepository;
 
 import org.apache.commons.lang.StringUtils;
 import org.meruvian.yama.persistence.EntityListWrapper;
-import org.meruvian.yama.persistence.LogInformation.StatusFlag;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -18,14 +17,12 @@ public class GoodReceivingRepository extends PersistenceRepository<GoodReceiving
 	public EntityListWrapper<GoodReceiving> findByKeyword(String keyword,
 			String order, String orderBy, int limit, int page, String condition) {
 
-		String criteria = "(th.title LIKE ? AND th.description LIKE ? "
-				+ "AND th.invoice.title LIKE ?)";
+		String criteria = "(th.invoiceNo LIKE ? AND th.businessPartner.name LIKE ? )";
 		criteria = criteria.replace("AND", condition);
-		criteria += " AND th.logInformation.statusFlag = ? ORDER BY "
+		criteria += " ORDER BY "
 				+ StringUtils.defaultIfEmpty(order, "th.id") + " " + orderBy;
-		Object[] params = { keyword, keyword, keyword,
-				StatusFlag.ACTIVE };
-		for (int i = 0; i < params.length - 1; i++) {
+		Object[] params = { keyword, keyword };
+		for (int i = 0; i < params.length; i++) {
 			if (params[i] instanceof String || params[i] == null) {
 				params[i] = StringUtils.defaultIfEmpty((String) params[i], "");
 				params[i] = StringUtils.join(new String[] { "%",
