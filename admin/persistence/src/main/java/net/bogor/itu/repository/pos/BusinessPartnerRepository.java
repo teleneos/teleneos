5aab4cf90011cbdb06ada9ed4795f91d9d17bf38
Dias Arifin <diaxz@madcoder.org>
2013-04-25 11:19:31 +0000

    resolve JERUKKEPOK-64
    git-svn-id: http://www.madcoder.org/svn/jerukkeprok/trunk@138 0f5c9f49-3e71-46a8-8949-fce9018a0e12

--- admin/persistence/src/main/java/net/bogor/itu/repository/pos/BusinessPartnerRepository.java
+++ admin/persistence/src/main/java/net/bogor/itu/repository/pos/BusinessPartnerRepository.java
@@ -1,7 +1,7 @@
 package net.bogor.itu.repository.pos;
 
 import net.bogor.itu.entity.pos.BusinessPartner;
-import net.bogor.itu.entity.pos.ItemCategory;
+import net.bogor.itu.entity.pos.BusinessPartner.Category;
 import net.bogor.itu.persistence.PersistenceRepository;
 
 import org.apache.commons.lang.StringUtils;
@@ -20,14 +20,32 @@ public class BusinessPartnerRepository extends
 	public EntityListWrapper<BusinessPartner> findByKeyword(String keyword,
 			String order, String orderBy, int limit, int page, String condition) {
 
-		String criteria = "(i.category LIKE ? AND i.name LIKE ? AND i.officePhone LIKE ? "
-				+ "i.fax LIKE ? AND i.email LIKE ? AND "
+		String criteria = "( i.name LIKE ? AND i.officePhone LIKE ? "
+				+ "AND i.fax LIKE ? AND i.email LIKE ? AND "
 				+ "i.address LIKE ? AND i.city LIKE ? AND "
 				+ "i.zipCode LIKE ? AND i.country LIKE ? AND i.description LIKE ?)";
 		criteria = criteria.replace("AND", condition);
-		criteria += " AND i.logInformation.statusFlag = ? ORDER BY "
-				+ StringUtils.defaultIfEmpty(order, "i.id") + " " + orderBy;
-		Object[] params = { keyword, keyword,keyword, keyword,keyword, keyword,keyword, keyword,keyword, keyword, StatusFlag.ACTIVE };
+		Category category;
+		if (keyword.equalsIgnoreCase(Category.CUSTOMER.toString())) {
+			category = Category.CUSTOMER;
+			criteria += " OR i.category = '" + category.ordinal()
+					+ "' AND i.logInformation.statusFlag = ? ORDER BY "
+					+ StringUtils.defaultIfEmpty(order, "i.id") + " " + orderBy;
+		} else if (keyword.equalsIgnoreCase(Category.VENDOR.toString())) {
+			category = Category.VENDOR;
+			criteria += " OR i.category = '" + category.ordinal()
+					+ "' AND i.logInformation.statusFlag = ? ORDER BY "
+					+ StringUtils.defaultIfEmpty(order, "i.id") + " " + orderBy;
+			System.err.println(criteria);
+		} else {
+			category = null;
+			criteria += " AND i.logInformation.statusFlag = ? ORDER BY "
+					+ StringUtils.defaultIfEmpty(order, "i.id") + " " + orderBy;
+			System.err.println(criteria);
+		}
+		Object[] params = { keyword, keyword, keyword, keyword, keyword,
+				keyword, keyword, keyword, keyword, StatusFlag.ACTIVE };
+
 		for (int i = 0; i < params.length - 1; i++) {
 			if (params[i] instanceof String || params[i] == null) {
 				params[i] = StringUtils.defaultIfEmpty((String) params[i], "");
@@ -35,7 +53,6 @@ public class BusinessPartnerRepository extends
 						(String) params[i], "%" });
 			}
 		}
-
 		return findAll(limit, page, "i", criteria, params);
 	}
 
