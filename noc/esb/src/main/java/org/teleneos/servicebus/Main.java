7db5cdd781205c489b0d6d13809df2d2c34afbe7
Dian Aditya <dian.aditya@meruvian.org>
2013-06-07 10:53:51 +0000

    Add executable class
    git-svn-id: http://www.madcoder.org/svn/jerukkeprok/trunk@206 0f5c9f49-3e71-46a8-8949-fce9018a0e12

---
+++ noc/esb/src/main/java/org/teleneos/servicebus/Main.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.teleneos.servicebus;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Dian Aditya
+ * 
+ */
+public class Main {
+	public static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		LOG.info("Starting application");
+
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		LOG.info("Application started {}",
+				new Date(applicationContext.getStartupDate()));
+	}
+
+}
