e9d2e320fa50d43e87555239a2403347fd045cd9
Dias Arifin <diaxz@madcoder.org>
2013-06-13 14:36:53 +0000

    add validation when socket message null
    git-svn-id: http://www.madcoder.org/svn/jerukkeprok/trunk@240 0f5c9f49-3e71-46a8-8949-fce9018a0e12

--- tele/broker/src/main/java/org/teleneos/broker/Main.java
+++ tele/broker/src/main/java/org/teleneos/broker/Main.java
@@ -20,48 +20,49 @@ public class Main {
 			BufferedReader inFromClient = new BufferedReader(
 					new InputStreamReader(connectionSocket.getInputStream()));
 			String msg = inFromClient.readLine();
-			System.err.println("msg "+msg);
-			try {
-				JSONObject mapper = new JSONObject(msg);
-				if (mapper.get("type").equals("logout")) {
-					StringWriter mac = new StringWriter();
-					Process process = Runtime
-							.getRuntime()
-							.exec(new String[] { "bash", "-c",
-									"chilli_query list | grep ' "+mapper.get("user")+" ' | awk '{print $1}'" });
-					IOUtils.copy(process.getInputStream(), mac);
-					commands = "chilli_query logout " + mac.toString();
-					System.out.println("["+mapper.get("user")+"] executing "+commands);
-					Runtime.getRuntime().exec(new String[] { "bash", "-c", commands });
-					inFromClient.close();
-				} 
-				if (mapper.get("type").equals("login")) {
-					StringWriter users = new StringWriter();
-					Process processCheckUser = Runtime
-							.getRuntime()
-							.exec(new String[] { "bash", "-c",
-									"chilli_query list | grep ' "+mapper.get("user")+" ' | awk '{print $1}'" });
-					IOUtils.copy(processCheckUser.getInputStream(), users);
-					String [] usersArray = users.toString().split("\n");
-					for (String u : usersArray) {
-						commands = "chilli_query logout " + u;
-						System.out.println("Duplicate Entry ["+mapper.get("user")+"] executing "+commands);
+			if (msg != null) {
+				try {
+					JSONObject mapper = new JSONObject(msg);
+					if (mapper.get("type").equals("logout")) {
+						StringWriter mac = new StringWriter();
+						Process process = Runtime
+								.getRuntime()
+								.exec(new String[] { "bash", "-c",
+										"chilli_query list | grep ' "+mapper.get("user")+" ' | awk '{print $1}'" });
+						IOUtils.copy(process.getInputStream(), mac);
+						commands = "chilli_query logout " + mac.toString();
+						System.out.println("["+mapper.get("user")+"] executing "+commands);
 						Runtime.getRuntime().exec(new String[] { "bash", "-c", commands });
+						inFromClient.close();
+					} 
+					if (mapper.get("type").equals("login")) {
+						StringWriter users = new StringWriter();
+						Process processCheckUser = Runtime
+								.getRuntime()
+								.exec(new String[] { "bash", "-c",
+										"chilli_query list | grep ' "+mapper.get("user")+" ' | awk '{print $1}'" });
+						IOUtils.copy(processCheckUser.getInputStream(), users);
+						String [] usersArray = users.toString().split("\n");
+						for (String u : usersArray) {
+							commands = "chilli_query logout " + u;
+							System.out.println("Duplicate Entry ["+mapper.get("user")+"] executing "+commands);
+							Runtime.getRuntime().exec(new String[] { "bash", "-c", commands });
+						}
+						String username = mapper.getString("user");
+						commands = "chilli_query authorize ip "+ mapper.get("ip") + 
+								" username " + username + 
+								" maxbwdown " + mapper.get("maxbwdown")+ 
+								" maxbwup " + mapper.get("maxbwup");
+						Process process = Runtime.getRuntime().exec(
+								new String[] { "bash", "-c", commands });
+						StringWriter writer = new StringWriter();
+						IOUtils.copy(process.getInputStream(), writer);
+						System.out.println("["+mapper.get("user")+"] executing "+commands);
+						inFromClient.close();
 					}
-					String username = mapper.getString("user");
-					commands = "chilli_query authorize ip "+ mapper.get("ip") + 
-							" username " + username + 
-							" maxbwdown " + mapper.get("maxbwdown")+ 
-							" maxbwup " + mapper.get("maxbwup");
-					Process process = Runtime.getRuntime().exec(
-							new String[] { "bash", "-c", commands });
-					StringWriter writer = new StringWriter();
-					IOUtils.copy(process.getInputStream(), writer);
-					System.out.println("["+mapper.get("user")+"] executing "+commands);
-					inFromClient.close();
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
 		}
 	}
