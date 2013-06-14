1dfaa47ca35142f885a3fcbc505261cacd4e4046
Dian Aditya <dian.aditya@meruvian.org>
2013-06-14 06:32:00 +0000

    Add accounting data synchronization capability
    git-svn-id: http://www.madcoder.org/svn/jerukkeprok/trunk@241 0f5c9f49-3e71-46a8-8949-fce9018a0e12

--- tele/servicebus/src/main/java/org/teleneos/servicebus/radius/AccountingProcessor.java
+++ tele/servicebus/src/main/java/org/teleneos/servicebus/radius/AccountingProcessor.java
@@ -8,7 +8,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.transaction.annotation.Transactional;
 import org.teleneos.radius.accounting.Radacct;
 import org.teleneos.radius.accounting.RadacctService;
 
@@ -16,7 +20,7 @@ import org.teleneos.radius.accounting.RadacctService;
  * @author Dian Aditya
  * 
  */
-public class AccountingProcessor {
+public class AccountingProcessor implements Processor {
 	@Inject
 	private RadacctService radacctService;
 
@@ -27,5 +31,15 @@ public class AccountingProcessor {
 		long then = calendar.getTimeInMillis();
 
 		return radacctService.findByRange(then, now);
+	}
+
+	@Override
+	@Transactional
+	public void process(Exchange exchange) throws Exception {
+		Message message = exchange.getIn();
+		Radacct radacct = message.getBody(Radacct.class);
+
+		int version = ObjectUtils.defaultIfNull(radacct.getSyncVersion(), 0);
+		radacct.setSyncVersion(version + 1);
 	}
 }
